fc97a1d56e4926c440397513335f993befbec55c
Mallow <mencheliu@gmail.com>
2025-11-29 16:39:12 +0800

    filter

--- script.js
+++ script.js
@@ -2,61 +2,169 @@ const greetingView = document.getElementById("greeting-view");
 const maleView = document.getElementById("male-view");
 const femaleView = document.getElementById("female-view");
 const bothView = document.getElementById("both-view");
+const profileGrid = document.getElementById("profile-grid");
 
 const btnBack = document.getElementById("btnBack");
+const btnAgain = document.getElementById("btnAgain");
 const btnMale = document.getElementById("btnMale");
 const btnFemale = document.getElementById("btnFemale");
 const btnBoth = document.getElementById("btnBoth");
 
 let viewControl = null;
 
+let userQueue = [];
+let currentIndex = 0;
+
+//轉頁面
 const exitGreeting = () => {
   greetingView.classList.add("hidden");
   btnBack.classList.remove("hidden");
+  btnAgain.classList.remove("hidden");
+
+  fetchUsers();
 };
 
 const returnGreeting = () => {
   greetingView.classList.remove("hidden");
   btnBack.classList.add("hidden");
+  btnAgain.classList.add("hidden");
+
   viewControl = null;
 };
 
+//按下性別按鈕
 btnMale.addEventListener("click", () => {
   viewControl = "male";
   exitGreeting();
-  maleView.classList.remove("hidden");
 });
 
 btnFemale.addEventListener("click", () => {
   viewControl = "female";
   exitGreeting();
-  femaleView.classList.remove("hidden");
 });
 
 btnBoth.addEventListener("click", () => {
   viewControl = "both";
   exitGreeting();
-  bothView.classList.remove("hidden");
 });
 
+//按下返回按鈕
 btnBack.addEventListener("click", () => {
+  profileGrid.innerHTML = "";
+
   switch (viewControl) {
     case "male":
-      maleView.classList.add("hidden");
       returnGreeting();
       break;
 
     case "female":
-      femaleView.classList.add("hidden");
       returnGreeting();
       break;
 
     case "both":
-      bothView.classList.add("hidden");
       returnGreeting();
       break;
 
     default:
-      console.log("Page Switch Error");
+      console.error("Page Switch Error");
   }
 });
+
+//按下重新載入按鈕
+btnAgain.addEventListener("click", () => {
+  userQueue = [];
+  currentIndex = 0;
+
+  fetchUsers();
+});
+
+//抓資料
+const fetchUsers = async () => {
+  let apiURL = "https://randomuser.me/api/?results=24";
+
+  if (viewControl == "male" || viewControl == "female") {
+    apiURL += `&gender=${viewControl}`;
+  } else if (viewControl == "both") {
+    apiURL = "https://randomuser.me/api/?results=24";
+  } else {
+    console.error("Gender Select Error");
+  }
+
+  try {
+    const response = await fetch(apiURL);
+
+    const data = await response.json();
+    userQueue = data.results;
+    currentIndex = 0;
+
+    console.log(viewControl);
+    console.log(userQueue);
+  } catch (error) {
+    console.error("資料讀取失敗", error);
+  } finally {
+    renderUserProfile();
+  }
+};
+
+//顯示使用者檔案
+const renderUserProfile = () => {
+  profileGrid.innerHTML = "";
+
+  if (userQueue.length == 0) {
+    console.error("Fetch Profile Error");
+  } else {
+    for (const user of userQueue) {
+      console.log(user);
+
+      const newCard = document.createElement("div");
+      newCard.classList.add(
+        "border",
+        "border-pink-200",
+        "px-4",
+        "py-2",
+        "rounded-md",
+        "flex",
+        "flex-col",
+        "jusitfy-center",
+        "items-center"
+      );
+
+      if (viewControl == "both") {
+        newCard.innerHTML = `
+      <img src="${user.picture.large}" alt="${user.name.first}" class="w-36">
+      <p class="text-lg font-bold">${user.name.first} ${user.name.last}</p>
+
+      <div class="flex flex-col w-full">
+        <p class="mt-2">
+          <span class="font-bold">Age:</span>
+          ${user.dob.age}
+        </p>
+        <p class="mt-1">
+          <span class="font-bold">Gender:</span>
+          ${user.gender}
+         </p>
+        <p class="mt-1">
+          <span class="font-bold">Location: </span>
+          ${user.location.city}, ${user.location.country}
+        </p>
+      </div>
+      `;
+      } else {
+        newCard.innerHTML = `
+      <img src="${user.picture.large}" alt="${user.name.first}" class="w-36">
+      <p class="text-lg font-bold">${user.name.first} ${user.name.last}</p>
+
+      <div class="flex flex-col w-full">
+        <p class="mt-2"><span class="font-bold">Age:</span> ${user.dob.age}</p>
+        <p class="mt-1">
+          <span class="font-bold">Location: </span>
+      ${user.location.city}, ${user.location.country}
+        </p>
+      </div>
+      `;
+      }
+
+      profileGrid.appendChild(newCard);
+    }
+  }
+};
